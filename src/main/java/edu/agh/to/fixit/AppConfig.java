10627625bb478cf0cf745155715f38da23035c8c
Mariusz Kuligowski <kulig15@gmail.com>
2016-05-03 13:09:23 +0200

    Added bootstrap plugin and Application Configuration

---
+++ src/main/java/edu/agh/to/fixit/AppConfig.java
@@ -0,0 +1,15 @@
+package edu.agh.to.fixit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class,args);
+    }
+
+}
