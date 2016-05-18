c3d74a7d34488190e523bb663c76902f628976f8
Mariusz Kuligowski <kulig15@gmail.com>
2016-05-18 20:49:03 +0200

    Added Session Factory Bean

--- src/main/java/edu/agh/to/fixit/AppConfig.java
+++ src/main/java/edu/agh/to/fixit/AppConfig.java
@@ -1,7 +1,12 @@
 package edu.agh.to.fixit;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
@@ -12,4 +17,10 @@ public class AppConfig {
         SpringApplication.run(AppConfig.class,args);
     }
 
+    @Bean
+    public SessionFactory sessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    }
 }
